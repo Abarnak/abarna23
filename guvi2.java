1733533899b33f181baf49ce3ff1dc92ef025baf
Abarnak <abarnakalaivendhan@gmail.com>
2017-08-09 21:02:29 +0530

    Add files via upload

---
+++ guvi2.java
@@ -0,0 +1,20 @@
+package aba;
+
+import java.util.Scanner;
+
+public class guvi2 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner u=new Scanner(System.in);
+		n=u.nextInt();
+		if(n%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+		}
+
+	}
+
+}
