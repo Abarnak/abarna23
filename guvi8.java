511f881c0a8329001fbbe63fc08f50a512f469d3
Abarnak <abarnakalaivendhan@gmail.com>
2017-08-12 17:24:37 +0530

    Add files via upload

---
+++ guvi8.java
@@ -0,0 +1,20 @@
+package aba;
+
+import java.util.Scanner;
+
+public class guvi8 {
+
+	public static void main(String[] args) {
+		int n=5;
+		Scanner y=new Scanner(System.in);
+		int x=y.nextInt();
+		for(int i=0;i<=n;i++){
+			System.out.println("true");
+		}
+	   if(n>5)
+		System.out.println("false");
+		}
+
+	}
+
+
