46549e7cdf4df321801f6e010b2bda917d7cde14
Abarnak <abarnakalaivendhan@gmail.com>
2017-08-12 18:58:25 +0530

    Add files via upload

---
+++ guvi11.java
@@ -0,0 +1,16 @@
+package gghh;
+
+public class guvi11 {
+
+	public static void main(String[] args) {
+		char a='s';
+		char b='c';
+		char t;
+		
+		t=a;a=b;b=t;
+	    
+		System.out.println(a+" "+b);
+
+	}
+
+}
