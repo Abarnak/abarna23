89986363c5ae22a01c371e74b48d67c5af234959
Abarnak <abarnakalaivendhan@gmail.com>
2017-08-12 17:53:42 +0530

    Add files via upload

---
+++ intointo.java
@@ -0,0 +1,15 @@
+package aba;
+
+import java.util.Scanner;
+
+public class intointo {
+
+	public static void main(String[] args) {
+		Scanner q=new Scanner(System.in);
+		String e=q.next();
+		
+		String a=new StringBuffer(e).reverse().toString();
+     System.out.println(a);
+	}
+
+}
