445cca3f0eb0c4fb1c925b9909fffc24ed3ee43c
Abarnak <abarnakalaivendhan@gmail.com>
2017-08-12 14:03:53 +0530

    Add files via upload

---
+++ guvi5.java
@@ -0,0 +1,17 @@
+package aba;
+
+import java.util.Scanner;
+
+public class guvi5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner t=new Scanner(System.in);
+String n=t.next();
+////StringBuffer v=n.reverse();
+int v=Integer.parseInt(n);
+System.out.println(v);
+
+	}
+
+}
