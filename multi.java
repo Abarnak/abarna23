2bd0d36c5201d18ac587f58c6cd6d25d951564fb
Abarnak <abarnakalaivendhan@gmail.com>
2017-10-01 14:36:10 +0530

    Add files via upload

---
+++ multi.java
@@ -0,0 +1,22 @@
+package A;
+
+import java.util.Scanner;
+
+public class multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.next();
+String b=s.next();
+String g="";
+int y=0;
+int p=Integer.parseInt(a);
+int l=Integer.parseInt(b);
+y=p*l;
+ g=Integer.toString(y);
+System.out.println(g);
+	}
+
+	}
+
